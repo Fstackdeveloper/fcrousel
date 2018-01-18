b87f57cd40acaa28506b4641bf18fbd845d0790a
Mohamed Al Moelef <mohamedalmoelef@Mohameds-iMac.local>
2018-01-19 02:37:46 +0300

    compelete drag click events

--- src/js/fcrousel.new.js
+++ src/js/fcrousel.new.js
@@ -18,7 +18,6 @@ import {link} from './items/fcrousel.link.js';
 import {product} from './items/fcrousel.product.js';
 import {service} from './items/fcrousel.service.js';
 import {offer} from './items/fcrousel.offer.js';
-import {drag} from './fcrousel.drag.js';
 
 
 
@@ -30,7 +29,6 @@ $.fn.fcarouselNew = function() {
  var fcrousel = $(this);
  fcrousel.settings = new setting($(this).data()).get();
  fcrousel.items =  (new items(fcrousel)).get();
- var paginate = new pagination(fcrousel);
  var size = new sizing(fcrousel);
  var fdesign = new design(fcrousel);
  fdesign.buttonsWithWarpper();
@@ -69,173 +67,10 @@ $.fn.fcarouselNew = function() {
 });
 
 
-       /*  
-           fcrousel.find( ".fcarousel-item" ).on("mousedown", function(e) 
-            {
-               e.stopPropagation();
-               if(e.which === 1) 
-               {
-                let inner = fcrousel.find(".fcarousel-inner" ).eq(0);
-                let itemsPerPage = fcrousel['itemsPerPage'];
-                let itemsPerPageWidth = inner.find( ".fcarousel-item" ).eq(0).outerWidth() * itemsPerPage;
-                let leftElm =Number(inner.css("left").replace('px', ''));
-                let leftFirst = e.clientX;   
-                let elmWidth = inner.find( ".fcarousel-item" ).eq(0).outerWidth();    
-                let leftSecond = e.clientX;
-
-                let length = fcrousel.find(".fcarousel-item" ).length;
-                
-                for(let i = (length-1); i >= (length - itemsPerPage); i--)
-                {
-                    let cloned = inner.find( ".fcarousel-item" ).eq(length-1).clone(true);
-                    cloned.addClass('fcarousel-item-copy');
-                    inner.prepend(cloned);
-                }
-                
-                
-                for(let i = itemsPerPage; i < 2*itemsPerPage; i++)
-                {
-                   let cloned = inner.find( ".fcarousel-item" ).eq(i).clone(true);
-                   cloned.addClass('fcarousel-item-copy');
-                   inner.append(cloned);
-                }
-                
-
-                let NextWarpWidth = fcrousel.find('.fcarousel-control-next-wrapper').eq(0).outerWidth();
-                let ItemMarginLeft =   Number(fcrousel.find('.fcarousel-item').css("marginLeft").replace('px', ''));
-                let ItemMarginRight =   Number(fcrousel.find('.fcarousel-item').css("marginRight").replace('px', ''));            
-                leftElm = leftElm - itemsPerPageWidth -  NextWarpWidth + ItemMarginLeft + ItemMarginRight;
-                
-                inner.css('left', leftElm);
-                
-                inner.css('width',((2*itemsPerPageWidth) + inner.width()));
-
-
-                $(this).on("mouseup", function(e) {
-                   
-                    e.stopPropagation();
-                    let x = (leftSecond - leftFirst) + (leftElm +  itemsPerPageWidth);     
-                    let elmNo = Math.round(x/elmWidth);
-                    
-                    
-                                  
-                    inner.find('.fcarousel-item-copy').remove();
-                    
-                    
-                    let SecondCopy = fcrousel;
-                    
-                    if(x > 0)
-                    {
-                        var length = SecondCopy.find(".fcarousel-item" ).length;
-                        for(let i = length; i > (length - elmNo); i--)
-                        {
-                          inner.prepend(inner.find( ".fcarousel-item" ).eq(length-1));
-                        }
-                    }
-                    else
-                    {
-                        
-                        for(let i = 0; i < (elmNo * -1); i++)
-                        {
-                            
-                           inner.append(inner.find( ".fcarousel-item" ).eq(0));
-                        }
-
-                    }
-                    
-                      inner.css('left',0); 
-                      $(this).off("mousemove");
-                      $(this).off("mouseup");
-                      
-                      return false;
-                  });  
-                  
-		 $(this).on("mousemove", function(e) {
-                    if(e.which === 1) 
-                    {
-                    e.stopPropagation();
-
-                    leftSecond = e.clientX;  
-                    
-                    let x = (leftSecond - leftFirst) + leftElm;     
-                    
-                     inner.css('left',x);    
-                  
-                  }
-                  else
-                  {
-                      $(this).trigger( "mouseup" );
-                  } 
-                    
-                });
-                
-                 $(this).on("mouseleave",function(e) { $(this).trigger( "mouseup" );});
-            }
-            });
-            
- 
-     
-      jQuery.event.special.touchstart = {
-    setup: function( _, ns, handle ){
-        if ( ns.includes("noPreventDefault") ) {
-            this.addEventListener("touchstart", handle, { passive: false });
-        } else {
-            this.addEventListener("touchstart", handle, { passive: true });
-        }
-    }
-};
-
-
- jQuery.event.special.touchmove = {
-    setup: function( _, ns, handle ){
-        if ( ns.includes("noPreventDefault") ) {
-            this.addEventListener("touchmove", handle, { passive: false });
-        } else {
-            this.addEventListener("touchmove", handle, { passive: true });
-        }
-    }
-};
 
 
 
-      jQuery.event.special.touchend = {
-    setup: function( _, ns, handle ){
-        if ( ns.includes("noPreventDefault") ) {
-            this.addEventListener("touchend", handle, { passive: false });
-        } else {
-            this.addEventListener("touchend", handle, { passive: true });
-        }
-    }
-};
-
-
-
-           fcrousel.on("touchstart", function(e) 
-            {
-                let leftElm =Number($(this).find('.fcarousel-inner').eq(0).css("left").replace('px', ''));
-                let leftFirst = e.originalEvent.changedTouches[0].clientX; 
-                
-                
-		fcrousel.on("touchmove", function(e) {
-                    e.stopPropagation();
-                    let leftSecond = e.originalEvent.changedTouches[0].clientX;  
-                    
-                    let x = (leftSecond - leftFirst) + leftElm; 
-                    
-                  $(this).find(".fcarousel-inner" ).css('left',x);    
-                 
-                  $("body").on("touchend", function(e) {
-                       e.stopPropagation();
-                      fcrousel.off("touchmove");
-                  });  
-                    
-                });
-            });
-
-     */
-    
-    paginate.click();
-    paginate.swipe();
+new pagination(fcrousel);
 
         
 				
@@ -247,7 +82,6 @@ $.fn.fcarouselNew = function() {
         $( window ).resize( function (e) { e.preventDefault(); size.resize(); });
         
         
-        new drag(fcrousel);
         
         
         //Border Radius Fcrousel Items
--- src/js/fcrousel.pagination.js
+++ src/js/fcrousel.pagination.js
@@ -5,112 +5,369 @@
     Website    : fullstackdeveloper.org
 */
 
+
 export class pagination {
-   waitAnimation = false;
-   autoMove = null;
-   fcarousel = null;
-   move = false;
-   
-   constructor(fcarousel)
-   {
-       this.fcarousel = fcarousel;
-       this.move = fcarousel.settings.move;
-       	//Auto Move	
-        this.startAutoMove();
-   }
-   
+    
+   fcrousel;
+   itemsPerPageWidth;
+   itemsPerPage;
+   LeftInnerWarp;
+   LeftInnerWarpAfter;
+   ItemWidth;
+   mouseX;
+   mouseXAfter;
+   ItemsCount;
+   inner;
+   innerWidth;
+   innerWidthAfter;
+   dragItemsCount;
+   DragDirection;
+
+   //Animated Pagination
+   stopOtherPaginationRequests = false;
+   autoMove = false;
+    
+   constructor(fcrousel)
+    {
+       this.fcrousel = fcrousel;
+       this.autoMove = this.fcrousel.settings.move;
+       this.startAutoMove();      
+       this.setJqueryPassiveEvent('touchstart');
+       this.setJqueryPassiveEvent('touchmove');
+       this.setJqueryPassiveEvent('touchend');
+       this.mouseDown();
+       this.touchStart();
+       this.click();
+    }
+        
+        
    // Auto Pagination
    startAutoMove()
    {
        	var rootThis = this;
-        if(this.move) {  
-            this.autoMove = setInterval(function (){rootThis.next();}, 4000); 
+        if(this.autoMove) {  
+            this.fcrousel['moveInterval'] = setInterval(function (){rootThis.next();}, 4000); 
         }
    }
    
-   
+    
+    setJqueryPassiveEvent(event)
+    {
+         jQuery.event.special[event] = {
+            setup: function( _, ns, handle ){
+                if ( ns.includes("noPreventDefault") ) {
+                    this.addEventListener(event, handle, { passive: false });
+                } else {
+                    this.addEventListener(event, handle, { passive: true });
+                }
+            }
+        };
+    }
+    
+    
+    setVariables()
+    {
+       this.inner = this.fcrousel.find(".fcarousel-inner" ).eq(0);
+       this.ItemWidth = this.inner.find( ".fcarousel-item" ).eq(0).outerWidth(true);
+       this.itemsPerPage = this.fcrousel['itemsPerPage'];
+       this.itemsPerPageWidth =  this.ItemWidth * this.itemsPerPage;
+       this.LeftInnerWarp =Number(this.inner.css("left").replace('px', ''));
+       this.ItemsCount = this.inner.find(".fcarousel-item" ).length;
+       this.innerWidth = this.inner.width();
+    }
+
+    setInnerLeft()
+    {
+        let NextWarpWidth = this.fcrousel.find('.fcarousel-control-next-wrapper').eq(0).outerWidth();
+        let ItemMarginLeft =   Number(this.inner.find('.fcarousel-item').css("marginLeft").replace('px', ''));
+        let ItemMarginRight =   Number(this.inner.find('.fcarousel-item').css("marginRight").replace('px', ''));            
+        this.LeftInnerWarpAfter = (this.LeftInnerWarp - this.itemsPerPageWidth);
+        this.inner.css('left', this.LeftInnerWarpAfter);
+    }
+    
+    removeInnerLeft()
+    {
+        this.inner.css('left', this.LeftInnerWarp);
+    }
+    
+    
+    setInnerWidth()
+    {
+        this.innerWidthAfter = (2*this.itemsPerPageWidth) + this.innerWidth;
+        this.inner.css('width',this.innerWidthAfter);
+    }
+    
+    
+    removeInnerWidth()
+    {
+        this.inner.css('width',this.innerWidth);
+    }
+    
+    
+    
+    copyItemsLeftRight()
+    {                
+                for(let i = (this.ItemsCount-1); i >= (this.ItemsCount - this.itemsPerPage); i--)
+                {
+                    let cloned = this.inner.find( ".fcarousel-item" ).eq(this.ItemsCount-1).clone(true);
+                    cloned.addClass('fcarousel-item-copy');
+                    this.inner.prepend(cloned);
+                }
+                
+                
+                for(let i = this.itemsPerPage; i < 2*this.itemsPerPage; i++)
+                {
+                   let cloned = this.inner.find( ".fcarousel-item" ).eq(i).clone(true);
+                   cloned.addClass('fcarousel-item-copy');
+                   this.inner.append(cloned);
+                }
+                
+    }
+    
+    
+    removeItemsLeftRight()
+    {
+         this.inner.find('.fcarousel-item-copy').remove();
+    }
+    
+    
+    
+    dragItemsCountDirection()
+    {
+         let left = (this.mouseXAfter - this.mouseX) ;
+         this.dragItemsCount = Math.round(left/this.ItemWidth);
+         this.DragDirection = (this.dragItemsCount > 0)?'right':'left';         
+         this.dragItemsCount = (this.dragItemsCount > 0)?this.dragItemsCount:(this.dragItemsCount*-1);         
+    }
+    
+    
+    removeEvents(item)
+    {
+        item.off("mousemove");
+        item.off("mouseup");
+        item.off("mouseleave");
+        item.off("touchmove");
+        item.off("touchend");
+    }
+    
+    
+    dragMoveEnd()
+    {
+        let rootThis = this;
+        let x = (this.mouseXAfter - this.mouseX) ;
+        let dragItemsCount = Math.round(x/this.ItemWidth);
+        let left = (dragItemsCount * this.ItemWidth) + this.LeftInnerWarpAfter;
+        
+        this.inner.animate({left:left},150, function(){
+            rootThis.dragItemsCountDirection();
+            rootThis.removeItemsLeftRight();    
+            rootThis.removeInnerLeft();
+            rootThis.removeInnerWidth();
+            rootThis[rootThis.DragDirection]();   
+            rootThis.stopOtherPaginationRequests = false; 
+        });
+        
+    }
+    
+    
+    mouseDown()
+    {
+            let rootThis = this;
+            this.fcrousel.find( ".fcarousel-item" ).on("mousedown", function(e) {  
+                if(e.which === 1) 
+                {
+                    if (!rootThis.stopOtherPaginationRequests)
+                    {
+                        e.preventDefault();
+                        if(rootThis.autoMove) { clearInterval(rootThis.fcrousel['moveInterval']); }
+                        rootThis.stopOtherPaginationRequests = true;
+                        rootThis.setVariables();
+                        rootThis.mouseX = e.clientX;
+                        rootThis.mouseXAfter = e.clientX;
+                        rootThis.copyItemsLeftRight();
+                        rootThis.setInnerLeft();
+                        rootThis.setInnerWidth();            
+                        rootThis.mouseMove($(this));
+                        rootThis.mouseUp($(this));
+                        rootThis.mouseLeave($(this));
+                    }
+                }
+            });
+       
+    }
+    
+    
+    
+    mouseMove(item)
+    {
+        let rootThis = this;
+        item.on("mousemove", function(e) {
+            if(e.which === 1) 
+            {
+                e.preventDefault();
+                rootThis.mouseXAfter = e.clientX;     
+                let left = (rootThis.mouseXAfter - rootThis.mouseX) + rootThis.LeftInnerWarpAfter;   
+                rootThis.inner.css('left',left);    
+            }
+            else
+            {
+              $(this).trigger( "mouseup" );
+            } 
+        });
+
+    }
+    
+    
+    
+    mouseUp(item)
+    {
+        let rootThis = this;
+        item.on("mouseup", function(e) {
+            
+            rootThis.dragMoveEnd();
+            rootThis.removeEvents($(this));             
+        });
+    }
+    
+    
+    
+    mouseLeave(item)
+    {
+        item.on("mouseleave",function(e) { 
+            $(this).trigger( "mouseup" );
+        });
+    }
+    
+    
+    touchStart()
+    {
+        
+            let rootThis = this;
+            this.fcrousel.find( ".fcarousel-item" ).on("touchstart", function(e) {  
+                if (!rootThis.stopOtherPaginationRequests)
+                {
+                    if(rootThis.autoMove) { clearInterval(rootThis.fcrousel['moveInterval']); }
+                    rootThis.stopOtherPaginationRequests = true;
+                    rootThis.setVariables();
+                    rootThis.mouseX = e.originalEvent.changedTouches[0].clientX;
+                    rootThis.mouseXAfter = e.originalEvent.changedTouches[0].clientX;  
+                    rootThis.copyItemsLeftRight();
+                    rootThis.setInnerLeft();
+                    rootThis.setInnerWidth();            
+                    rootThis.touchMove($(this));
+                    rootThis.touchEnd($(this));
+                }
+            });
+      
+
+    }
+    
+    
+    
+   touchMove(item)
+    {
+        let rootThis = this;
+        item.on("touchmove", function(e) {
+            rootThis.mouseXAfter = e.originalEvent.changedTouches[0].clientX;                    
+            let left = (rootThis.mouseXAfter - rootThis.mouseX) + rootThis.LeftInnerWarpAfter; 
+            rootThis.inner.css('left',left);    
+        });
+
+    }
+    
+    
+    
+    touchEnd(item)
+    {
+        let rootThis = this;
+        item.on("touchend", function(e) {
+            rootThis.dragMoveEnd();
+            rootThis.removeEvents($(this));             
+        });
+    }
+    
+    
+    
+    
    //Pagination By Click (Add Events)
    click()
    {
         // On Click Events (Next Prev)
-	this.fcarousel.find( ".fcarousel-control-prev" ).on('click', { rootThis : this }, function(e)
+	this.fcrousel.find( ".fcarousel-control-prev" ).on('click', { rootThis : this }, function(e)
             {
 		e.data.rootThis.prev();
             });
 
-	this.fcarousel.find( ".fcarousel-control-next" ).on('click', { rootThis : this }, function(e)
+	this.fcrousel.find( ".fcarousel-control-next" ).on('click', { rootThis : this }, function(e)
             {
                 e.data.rootThis.next();
             });
    }
-   
-   
-   //Pagination By swipe (for Mobile) (Add Events)
-   swipe()
-   {
-     /*
-       var rootThis = this;
-      	//On Swipe Events (for Mobile)
-	Hammer(this.fcarousel[ 0 ]).on("swipeleft", function(e)
-            {
-		rootThis.prev();   
-	    });		
-	
-        Hammer(this.fcarousel[ 0 ]).on("swiperight", function(e) 
-            {
-		rootThis.next();
-            });
-            
-            this.fcarousel.find('img, a').each(function() {
-                Hammer($(this)[ 0 ]).on("swipeleft", function(e)
-                    {
-                        rootThis.prev();   
-                    });		
 
-                Hammer($(this)[ 0 ]).on("swiperight", function(e) 
-                    {
-                        rootThis.next();
-                    });     
-            });
-            */
-   }
-   
 
-//Next Pages
+
+    
+    
+    
+//Next Pages (For Drag and Touch)    
+    right()
+    {
+        let rootThis = this;
+        for(let i = this.ItemsCount; i > (this.ItemsCount - this.dragItemsCount); i--)
+        {
+          this.inner.prepend(this.inner.find( ".fcarousel-item" ).eq(this.ItemsCount-1));
+        }
+        if(this.autoMove) { this.fcrousel['moveInterval'] = setInterval(function (){rootThis.next();}, 4000); }
+    }
+    
+
+//Previous Pages (For Drag and Touch)    
+    left()
+    {
+        let rootThis = this;
+        for(let i = 0; i < this.dragItemsCount; i++)
+        {
+           this.inner.append(this.inner.find( ".fcarousel-item" ).eq(0));
+        }
+        if(this.autoMove) { this.fcrousel['moveInterval'] = setInterval(function (){rootThis.prev();}, 4000);}
+    }
+    
+    
+    
+
+ //Next Pages (For Click & AutoMove)    
     next()
 	{
             var rootThis = this;
-            if (!this.waitAnimation)
+            if (!this.stopOtherPaginationRequests)
 		{				 
-		if(this.move) { clearInterval(this.autoMove); }			
-		var length = this.fcarousel.find(".fcarousel-item" ).length;
+		if(this.autoMove) { clearInterval(this.fcrousel['moveInterval']); }			
+		var length = this.fcrousel.find(".fcarousel-item" ).length;
                 var target_item = length-1;
-		this.waitAnimation = true;
-		var vleft = this.fcarousel.find( ".fcarousel-item" ).eq(target_item).outerWidth();
-		this.fcarousel.find(".fcarousel-inner" ).prepend(this.fcarousel.find( ".fcarousel-item" ).eq(target_item)).css('left',-vleft).animate({left:0},500,function(){rootThis.waitAnimation = false;					
-		if(rootThis.move) { rootThis.autoMove = setInterval(function (){rootThis.next();}, 4000); }
+		this.stopOtherPaginationRequests = true;
+		var vleft = this.fcrousel.find( ".fcarousel-item" ).eq(target_item).outerWidth();
+		this.fcrousel.find(".fcarousel-inner" ).prepend(this.fcrousel.find( ".fcarousel-item" ).eq(target_item)).css('left',-vleft).animate({left:0},300,function(){rootThis.stopOtherPaginationRequests = false;					
+		if(rootThis.autoMove) { rootThis.fcrousel['moveInterval'] = setInterval(function (){rootThis.next();}, 4000); }
                 });}
         }
 
-//Previous Pages
+//Previous Pages (For Click & AutoMove)  
    prev()
 	{
             var rootThis = this;
-            if (!this.waitAnimation)
+            if (!this.stopOtherPaginationRequests)
 		{
-		if(this.move) { clearInterval(this.autoMove); }
+		if(this.autoMove) { clearInterval(this.fcrousel['moveInterval']); }
 		var target_item = 0;
-                this.waitAnimation = true;
-		var vleft = this.fcarousel.find( ".fcarousel-item" ).eq(target_item).outerWidth();
-		this.fcarousel.find(".fcarousel-inner" ).animate({left:-vleft},500,function(){
-		rootThis.waitAnimation = false; 
+                this.stopOtherPaginationRequests = true;
+		var vleft = this.fcrousel.find( ".fcarousel-item" ).eq(target_item).outerWidth();
+		this.fcrousel.find(".fcarousel-inner" ).animate({left:-vleft},300,function(){
+		rootThis.stopOtherPaginationRequests = false; 
 		$(this).css('left',0); 
-		$(this).append(rootThis.fcarousel.find( ".fcarousel-item" ).eq(target_item));
-		if(rootThis.move) { rootThis.autoMove = setInterval(function (){rootThis.prev();}, 4000);}
+		$(this).append(rootThis.fcrousel.find( ".fcarousel-item" ).eq(target_item));
+		if(rootThis.autoMove) { rootThis.fcrousel['moveInterval'] = setInterval(function (){rootThis.prev();}, 4000);}
             });}
         }
-        
-        
-        
 
+    
 }
